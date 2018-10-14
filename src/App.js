625ce9ba0aa23231df10a2159f73a363dd5009ed
sakhawatliv <sakhawatliv@gmail.com>
2018-10-14 11:17:44 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      items:[],
+      isLoaded:false,
+    }
+
+  }
+  componentDidMount(){
+    fetch('http://jsonplaceholder.typicode.com/users').
+    then(res=>res.json()).
+    then(json=>
+    {
+      this.setState({
+        isLoaded:true,
+        items:json,
+      })
+    });
+  }
+  
+  render() {
+    var {isLoaded,items} = this.state;
+    if(!isLoaded)
+    {
+      return <div>Loding.............</div>
+    }
+    else{
+      return (
+        <div className="App">
+          <ul>
+            {items.map(item=>(
+              <li key={item.key}>
+                Name : {item.name} | Email : {item.email}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+    
+  }
+}
+
+export default App;
